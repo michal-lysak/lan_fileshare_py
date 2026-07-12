b12d3518cc5d99c3afc5fb1149a4af76c5b1d450
Michal Lysak <michelmichy7@gmail.com>
2026-07-12 20:39:52 +0200

    added ignore for user's own computer

--- Backend/discovery.py
+++ Backend/discovery.py
@@ -12,16 +12,16 @@ class DiscoveryWorker(QObject):
     stateChanged = Signal(str)
     packetReceived_Signal = Signal(str, str, str)
     packetSend_Signal = Signal(str)
-    
+
 
     def __init__(self):
-        
+
         super().__init__()
         self.socket = QUdpSocket(self)
         self.devices = set()
 
     def start(self):
-        
+
         PORT = 9999
         # Bind UDP socket (Qt way)
         self.socket.bind(
@@ -68,25 +68,25 @@ class DiscoveryWorker(QObject):
     def conRequest(self, ip: str):
         hostname = socket.gethostname()
         message = f"CONNECTION_REQUEST|{hostname}".encode()
-        
+
         self.socket.writeDatagram(message, QHostAddress(ip), 9999)
 
- 
-        
+
+
     def processPendingDatagrams(self):
         while self.socket.hasPendingDatagrams():
             datagram, host, port = self.socket.readDatagram(
                 self.socket.pendingDatagramSize()
             )
 
+            my_hostname = socket.gethostname()
+
             message = datagram.data().decode()
             ip = host.toString()
             parts = message.split("|")
+            name = parts[1]
 
-            if message.startswith("DISCOVER"):
-
-                name = parts[1]
-
+            if message.startswith("DISCOVER") and name != my_hostname:
                 if ip not in self.devices:
                     self.devices.add(ip)
                     print(f"Found device: {name} -> {ip}")
@@ -105,3 +105,5 @@ class DiscoveryWorker(QObject):
                 packet_type = parts[0]
                 device_name = parts[1] if len(parts) > 1 else ""
                 self.packetReceived_Signal.emit(ip, packet_type, device_name or "")
+            else:
+                print("Ignored: Discovered myself")
