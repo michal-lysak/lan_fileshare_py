987cfbaed0d6e203fc0c5b6ed5a3118e26585b76
Michal Lysak <michelmichy7@gmail.com>
2026-07-28 22:15:45 +0200

    added clearing model function

--- backend/models/FileListModel.py
+++ backend/models/FileListModel.py
@@ -104,3 +104,9 @@ class FileListModel(QAbstractListModel):
                 self.endRemoveRows()
                 print(f"Removed file with id: {id}. Current model size: {self.rowCount()}")
                 return
+            
+    def clearModel(self):
+        self.beginResetModel()
+        self._files.clear()
+        self.endResetModel()
+        print("Cleared model. Current model size:", self.rowCount())
